6c3a83588dc040783ffc98aa33a7e9954f2e6104
Nabeel Mansour <mikaeel1@hotmail.com>
2024-12-05 15:33:31 +0100

    added styles to nav menu

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-// setup nav
-// const navBtn = document.getElementById("nav-btn");
-// const divider = document.getElementById("divider");
-// const form = document.getElementById("form");
-// const nav = document.getElementById("hero__nav");
-// const navClose = document.getElementById("nav-close");
-
-// show nav
-// navBtn.addEventListener("click", () => {
-//   nav.classList.add("showNav");
-//   form.style.display = "none";
-//   divider.style.display = "none";
-// });
-// close nav
-// navClose.addEventListener("click", () => {
-//   nav.classList.remove("showNav");
-// });
-// setup nav
 const navBtn = document.getElementById("nav-btn");
 const nav = document.getElementById("header__nav");
 const navClose = document.getElementById("nav-close");
@@ -23,7 +5,6 @@ const heroNavText = document.getElementById("hero__nav-text");
 
 // show nav
 navBtn.addEventListener("click", () => {
-  console.log("Next");
   nav.classList.add("showNav");
   heroNavText.classList.add("showNav");
 });
